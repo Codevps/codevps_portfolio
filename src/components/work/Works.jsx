007204b90f2b3ae9e77446004156677817f7633f
Pratham Sawant <codevps07@gmail.com>
2024-04-22 21:55:49 +0530

    working with portfolio adjustments

--- src/components/work/Works.jsx
+++ src/components/work/Works.jsx
@@ -9,11 +9,15 @@ const Works = () => {
   const [active, setActive] = useState(0);
 
   useEffect(() => {
-    const newProjects = projectsData.filter((project) => {
-      return project.category.toLowerCase() === item.name;
-    });
+    if (item.name === "all") {
+      setProjects(projectsData);
+    } else {
+      const newProjects = projectsData.filter((project) => {
+        return project.category.toLowerCase() === item.name;
+      });
 
-    setProjects(newProjects);
+      setProjects(newProjects);
+    }
   }, [item]);
 
   const handleClick = (e, index) => {
