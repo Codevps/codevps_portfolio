4981d005bfc256612dd253e4e1a7761a2714e515
Pratham Sawant <codevps07@gmail.com>
2024-05-20 08:30:43 +0530

    qualification done

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -117,6 +117,20 @@ const Qualification = () => {
                 </div>
               </div>
             </div>
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">App Development</h3>
+                <span className="qualification__subtitle">React Native </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2024
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
           </div>
 
           <div
